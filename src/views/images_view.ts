1ecaf1ca8ba03ef3c71cab4e99c17a9506fd38f2
Leonardo Ferreira <leonardo.ferreiraa2@gmail.com>
2024-07-31 13:53:48 -0300

    Add files via upload
    ajust

--- src/views/images_view.ts
+++ src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
     render(image: Image) {
         return{
             id: image.id,
-            url: `http://localhost:3333/uploads/${image.path}` 
+            url: `https://backend-ccer.vercel.app/uploads/${image.path}` 
         }
     },
 
