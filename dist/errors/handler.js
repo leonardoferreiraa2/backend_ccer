9f35e1dbbab061f61c3a598919c29627f90dac77
Leonardo Ferreira <leonardo.ferreiraa2@gmail.com>
2024-07-31 21:25:02 -0300

    Delete dist directory
    teste

--- dist/errors/handler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (error, request, response, next) => {
-    console.error(error);
-    return response.status(500).json({ error: error.message });
-};
-exports.default = errorHandler;
