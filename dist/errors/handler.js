81680e49b3286b40534b0209d3b1b09a290f56f7
Leonardo Ferreira <leonardo.ferreiraa2@gmail.com>
2024-07-31 21:04:01 -0300

    Add files via upload
    ajuste rrro

--- dist/errors/handler.js
+++ dist/errors/handler.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const errorHandler = (error, request, response, next) => {
     console.error(error);
-    return response.status(500).json({ message: 'Internal server error' });
+    return response.status(500).json({ error: error.message });
 };
 exports.default = errorHandler;
